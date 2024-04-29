0dc5a55af6714eb4c37883a495957dc2e47e71ba
Emmanuel Harley <mannyharley20@gmail.com>
2024-04-29 08:56:50 -0700

    correct signup

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -10,7 +10,6 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { Title } from "../_components/Typography";
 import ConfettiComp from "./_components/Confetti";
-import SignUpPage from "./_components/SignUpPage";
 
 const DynamicPinnedGoal = dynamic(() => import("./_components/DashGoalCard"), {
   loading: () => <Skeleton paragraph={{ rows: 4 }} />,
@@ -36,7 +35,7 @@ export default async function HomePage({
 }) {
   const session = await getServerSession(authOptions);
   if (!session) {
-    return <SignUpPage />;
+    redirect("/signup");
   }
   if (isExtendedSession(session)) {
     const onboardingProfileCount = await getOnboardingProfileCount(
