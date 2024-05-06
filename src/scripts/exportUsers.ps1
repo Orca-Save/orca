042571bb9f96cf6c9a11cef4c54f858343a687d4
Emmanuel <mannyharley20@gmail.com>
2024-05-06 14:17:14 -0500

    export users script complete!

--- src/scripts/exportUsers.ps1
+++ src/scripts/exportUsers.ps1
@@ -1,43 +1,52 @@
-Connect-AzureAD 
+# Set the Tenant ID
+$tenantId = "06187e97-276e-4e2e-b8c0-494f51cbad65"
 
-$users = Get-AzureADUser -All $true
-$userData = foreach ($user in $users) {
-    $UserName = if ($user.SignInNames.Value -contains $null) {
-        $user.UserPrincipalName
-    } else {
-        $user.SignInNames.Value
-    }
-    $userProperties = [ordered]@{
-        userPrincipalName = $user.UserPrincipalName
-        UserName = $UserName
-        displayName = $user.DisplayName
-        surname = $user.Surname
-        mail = $user.Mail
-        givenName = $user.GivenName
-        objectId = $user.ObjectId
-        userType = $user.UserType
-        jobTitle = $user.JobTitle
-        department = $user.Department
-        accountEnabled = $user.AccountEnabled
-        usageLocation = $user.UsageLocation
-        streetAddress = $user.StreetAddress
-        state = $user.State
-        country = $user.Country
-        physicalDeliveryOfficeName = $user.PhysicalDeliveryOfficeName
-        city = $user.City
-        postalCode = $user.PostalCode
-        telephoneNumber = $user.TelephoneNumber
-        mobile = $user.Mobile
-        authenticationPhoneNumber = $user.AuthenticationPhoneNumber
-        authenticationAlternativePhoneNumber = $user.AuthenticationAlternativePhoneNumber
-        authenticationEmail = $user.AuthenticationEmail
-        alternateEmailAddress = $user.AlternateEmailAddress
-        ageGroup = $user.AgeGroup
-        consentProvidedForMinor = $user.ConsentProvidedForMinor
-        legalAgeGroupClassification = $user.LegalAgeGroupClassification
-        
-    }
-    New-Object -TypeName PSObject -Property $userProperties
+# Authenticate to Azure with the specified tenant
+Connect-AzAccount -TenantId $tenantId
+
+# Get the access token
+$token = (Get-AzAccessToken -TenantId $tenantId -ResourceUrl "https://graph.microsoft.com/").Token
+
+# Set the API endpoint and headers
+$uri = "https://graph.microsoft.com/beta/users?`$top=999"
+$headers = @{
+    Authorization = "Bearer $token"
 }
 
-$userData | Export-Csv -Path ".\Test.csv" -NoTypeInformation
+# Initialize an array to hold user data
+$allUsers = @()
+
+# Make the REST API call
+do {
+    $response = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
+    $allUsers += $response.value
+    $uri = $response."@odata.nextLink"
+} while ($uri)
+
+# Function to extract the email address from identities
+function Get-EmailAddressFromIdentities($identities) {
+    foreach ($identity in $identities) {
+        if ($identity.signInType -eq "emailAddress") {
+            return $identity.issuerAssignedId
+        }
+    }
+    return $null
+}
+
+# Extract relevant fields into a more usable form
+$processedUsers = $allUsers | ForEach-Object {
+    [PSCustomObject]@{
+        Id                = $_.id
+        DisplayName       = $_.displayName
+        UserPrincipalName = $_.userPrincipalName
+        EmailAddress      = Get-EmailAddressFromIdentities($_.identities)
+    }
+}
+
+# Define the output CSV file path
+$outputCsvPath = "B2CUsers.csv"
+
+# Export the data to CSV
+$processedUsers | Export-Csv -Path $outputCsvPath -NoTypeInformation
+
+Write-Output "User data exported to: $outputCsvPath"
