0901325b5c457b02bccf786732fc28960d9502af
Emmanuel <mannyharley20@gmail.com>
2024-05-10 11:46:32 -0500

    update share link preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'Orca',
-  description: 'Impulse Save!',
+  description: 'Impulse saving.!',
 };
 
 export default function RootLayout({
@@ -31,6 +31,21 @@ export default function RootLayout({
           name='viewport'
           content='width=device-width, initial-scale=1, maximum-scale=1'
         />
+
+        <meta property='og:url' content='https://www.orca-money.com' />
+        <meta property='og:title' content='Orca | Impulse saving.' />
+        <meta
+          property='og:description'
+          content="Stop buying what you don't need so you can save for what you really want. Orca is the personal finance app that makes you better, not your budget."
+        />
+        <meta property='og:site_name' content='orca-money.com' />
+
+        <meta
+          property='og:image'
+          content='https://uploads-ssl.webflow.com/660d63c5bb6c967462fada51/6615fb420a726c9f1dba29f0_Frame%201.png'
+        />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
       </head>
       <body
         className={cn(
