cd3aa4fb6183530d063a2962348da0d9eb750fb1
Emmanuel Harley <mannyharley20@gmail.com>
2024-05-20 07:17:03 -0700

    excitement text

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -58,7 +58,7 @@ export default function SignUpPage() {
               onClick={onClick}
             >
               {/* <a href="https://orcanext.b2clogin.com/orcanext.onmicrosoft.com/b2c_1_orca_signin/oauth2/v2.0/authorize?client_id=3dd4e88e-63c3-49b3-af56-f2770cf498a8&scope=offline_access%20openid&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fazure-ad-b2c&state=AUwpUvWbqIRYKTlgIQ6LnrYpVQwob6tnGMSXe5RVHSI&option=signup"> */}
-              Lets do this.
+              Lets go!
               {/* </a> */}
             </Button>
             {/* <Button size="large" className="w-full " onClick={onClick}>
