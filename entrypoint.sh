25a3533939cfb3e201a52f4ee1b5d07d105fbf4c
Emmanuel Harley <mannyharley20@gmail.com>
2024-05-23 14:17:12 -0700

    simple env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
+set -e
 
 # Wait until PostgreSQL is ready
-until pg_isready -h db -U postgres; do
-  echo "Waiting for postgres..."
-  sleep 2
-done
+# until pg_isready -h db -U postgres; do
+#   echo "Waiting for postgres..."
+#   sleep 2
+# done
 
-# Run database migrations
-echo "Database is ready. Running migrations..."
-npx prisma migrate deploy 
-echo "Migrations complete."
+# Output all environment variables to .env file
+printenv > .env
+ls -al
+# # Run database migrations
+# echo "Database is ready. Running migrations..."
+# npx prisma migrate deploy 
+# echo "Migrations complete."
 
 # Start the application
 npm start
--- generate-env-file.sh
+++ generate-env-file.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-set -e
-
-# Output all environment variables to .env file
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
-
+printenv > .env
 # cd /etc/ssh/ \
 #     ssh-keygen -A
 
