b61cdf066d93ce9eab8d3dfd0a3cdab0ccad3a5e
Emmanuel Harley <mannyharley20@gmail.com>
2024-05-23 07:41:20 -0700

    debug

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,15 +11,20 @@ const openSans = Open_Sans({
   variable: '--font-opensans',
 });
 export default function SignUpPage() {
-  const onClick = () => signIn('azure-ad-b2c', { callbackUrl: baseURL });
+  const onClick = () => {
+    console.log(baseURL);
+    signIn('azure-ad-b2c', { callbackUrl: baseURL });
+  };
   return (
     <Layout>
       <div
         className='bg-color-black mg-5 flex justify-center items-center h-screen'
-        style={{ margin: 15 }}>
+        style={{ margin: 15 }}
+      >
         <Space direction='vertical' size={50}>
           <h1
-            className={`${openSans.className} text-center decoration-clone pb-3 text-6xl bg-clip-text text-transparent bg-gradient-to-r from-orca-blue to-orca-pink font-bold`}>
+            className={`${openSans.className} text-center decoration-clone pb-3 text-6xl bg-clip-text text-transparent bg-gradient-to-r from-orca-blue to-orca-pink font-bold`}
+          >
             Ready to save an extra $3,800 a year?
           </h1>
 
@@ -28,7 +33,8 @@ export default function SignUpPage() {
               type='primary'
               size='large'
               className='w-full'
-              onClick={onClick}>
+              onClick={onClick}
+            >
               {/* <a href="https://orcanext.b2clogin.com/orcanext.onmicrosoft.com/b2c_1_orca_signin/oauth2/v2.0/authorize?client_id=3dd4e88e-63c3-49b3-af56-f2770cf498a8&scope=offline_access%20openid&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fazure-ad-b2c&state=AUwpUvWbqIRYKTlgIQ6LnrYpVQwob6tnGMSXe5RVHSI&option=signup"> */}
               Sign Up / Login
               {/* </a> */}
