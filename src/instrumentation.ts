91213f45d77d5a39492e0839ed841f30bdedf444
Emmanuel Harley <mannyharley20@gmail.com>
2024-06-10 15:05:21 -0700

    cleanup

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,14 +1,8 @@
 export async function register() {
-  // if (process.env.NODE_ENV !== 'production') {
-  //   return;
-  // }
+  if (process.env.NODE_ENV !== 'production') {
+    return;
+  }
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./appInsights');
   }
-  // registerOTel({
-  //   serviceName: 'orca-local',
-  //   traceExporter: new AzureMonitorTraceExporter({
-  //     connectionString: process.env.NEXT_PUBLIC_APP_INSIGHTS_CONNECTION_STRING,
-  //   }),
-  // });
 }
