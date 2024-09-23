f762fa232568a4abd7af65161cca6efaa131a1b6
Emmanuel Harley <mannyharley20@gmail.com>
2024-09-22 20:45:04 -0700

    version increase and correction

--- client/src/components/charts/ChartPage.tsx
+++ client/src/components/charts/ChartPage.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from 'antd';
 import React from 'react';
 import {
   Bar,
@@ -11,6 +12,7 @@ import {
   XAxis,
   YAxis,
 } from 'recharts';
+
 import useFetch from '../../hooks/useFetch';
 
 const data = [
