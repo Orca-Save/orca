099877ee546dbc2acd5da18299964c59b1f0c87e
Your Name <your_email@example.com>
2025-01-10 11:04:57 -0800

    Enhanced protected route

--- client/src/components/routing/ProtectedRoute.tsx
+++ client/src/components/routing/ProtectedRoute.tsx
@@ -1,13 +1,20 @@
-import { useIsAuthenticated } from '@azure/msal-react';
 import React from 'react';
+import { Skeleton } from 'antd';
+import { useIsAuthenticated, useMsal } from '@azure/msal-react';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 const ProtectedRoute = () => {
   const isAuthenticated = useIsAuthenticated();
   const navigate = useNavigate();
 
+  const { accounts } = useMsal();
+  const userId = accounts[0]?.localAccountId;
   if (!isAuthenticated) {
     navigate('/sign-in');
+  }
+
+  if (!userId) {
+    return <Skeleton active />;
   }
 
   return <Outlet />;
